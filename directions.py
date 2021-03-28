8d38f66c2710f43233a210117c71860f706cf1b2
AayushPokharel <rockmanpokharel@yahoo.com>
2021-03-28 18:02:23 +0545

    v 1

--- directions.py
+++
@@ -1,7 +0,0 @@
-import os.path
-
-ProjectRoot = os.path.dirname(os.path.dirname(__file__))
-AbsoluteProjectRoot = os.path.abspath(ProjectRoot)
-
-if __name__ == "__main__":
-    print(AbsoluteProjectRoot)
--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(
-     name='directions',  
-     version='0.1',
-     scripts=['directions'] ,
-     author="Aayush Pokharel",
-     author_email="rockmanpokharel@yahoo.com",
-     description="A small custom made Library that gives various directory.",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/AayushPokharel/directions_pkg",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "Operating System :: OS Independent",
-     ],
- )
+    name='directionfinder',
+    version='0.1',
+    #scripts=['directions'] ,
+    author="Aayush Pokharel",
+    author_email="rockmanpokharel@yahoo.com",
+    description="A small custom made Library that gives various directory.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AayushPokharel/directions_pkg",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
