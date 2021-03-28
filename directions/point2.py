e75202978c8dcd5d9fa4c11528ca20ccd7d07254
AayushPokharel <rockmanpokharel@yahoo.com>
2021-03-28 21:04:44 +0545

    v4

--- directions/point2.py
+++ directions/point2.py
@@ -4,6 +4,7 @@ import os.path
 
 ProjectRoot = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 AbsoluteProjectRoot = os.path.abspath(ProjectRoot)
+ChromeDriverLocation = os.path.join(ProjectRoot,"WebDriver","chromedriver.exe")
 
 if __name__ == "__main__":
     print(AbsoluteProjectRoot)
